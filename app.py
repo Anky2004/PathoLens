fde2d4665a40d996c6e6199d5f3a7ef8f995fa7b
Anky2004 <saharaiankita@gmail.com>
2025-08-05 23:11:05 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template
+from PIL import Image
+import torch
+from torchvision import transforms, models
+
+app = Flask(__name__)
+
+# Load model
+model = models.resnet18()
+model.fc = torch.nn.Linear(model.fc.in_features, 4)
+model.load_state_dict(torch.load('blood_model.pth', map_location=torch.device('cpu')))
+model.eval()
+
+labels = ['Eosinophil', 'Lymphocyte', 'Monocyte', 'Neutrophil']
+
+def predict(image):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+    img = transform(image).unsqueeze(0)
+    with torch.no_grad():
+        outputs = model(img)
+        _, predicted = torch.max(outputs, 1)
+        return labels[predicted.item()]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def upload():
+    file = request.files['image']
+    image = Image.open(file.stream).convert('RGB')
+    label = predict(image)
+    return render_template('index.html', prediction=label)
+
+if __name__ == '__main__':
+    app.run(debug=True)
